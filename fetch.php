494b9aec444adecf5da6b00f5bb75b5a1e672b88
omkar-ghotekar <60335408+omkar-ghotekar@users.noreply.github.com>
2020-04-28 12:43:20 +0530

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,6 +1,7 @@
 <?php
 
-$connect = mysqli_connect("localhost", "root", "", "contact");
+$connect = mysqli_connect("111.118.215.98", "theervu_contact", "qwertyuiop1234567890", "theervu_contact");
+
 $sql = "SELECT * FROM contacttable";  
 $result = mysqli_query($connect, $sql);
 ?>
